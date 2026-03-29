86a13a573cba89ea79cfb2552de16b93d2f11d09
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-03-29 15:26:43 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+        in:
+            - str[] strs
+        out:
+            - group all anagrams into sublists str[[]]
+        constraints:
+            - 1 <= strs.length <= 1000
+            - 1 <= strs[i].length <= 1000
+            - strs[i] all lowercase letters
+        '''
+        anagrams = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                idx = ord(c) - ord('a')
+                count[idx] += 1
+            
+            anagrams[tuple(count)].append(s)
+
+        return list(anagrams.values())
