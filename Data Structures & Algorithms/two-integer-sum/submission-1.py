12beb21f8bd9d11b8791ccea02a3044fe0a9159e
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-03-29 14:05:36 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+        in:
+            - int[] nums
+            - int target
+        out:
+            - indices i, j s.t. 
+                nums[i] + nums[j] == target
+                and i != j
+        constraints:
+            -  each input has one valid pair of indices (i, j)
+        '''
+        numbers = dict()
+
+        for i in range(len(nums)):
+            num = nums[i]
+            complement = target - num
+
+            if complement in numbers:
+                return [numbers[complement], i]
+            
+            numbers[num] = i
+
+# Time complexity: O(n)
+# Space complexity: O(n)
