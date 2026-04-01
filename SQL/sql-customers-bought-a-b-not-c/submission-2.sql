aa23b33ee798d0a3a8dfeb0bde04de928a5707eb
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-04-01 10:47:32 -0400

    Add: sql-customers-bought-a-b-not-c - submission-2

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-2.sql
@@ -0,0 +1,26 @@
+-- Find customers who purchased both products 'A' and 'B', 
+-- but have never purchased product 'C'.
+WITH ab_customers AS (
+    -- Customers who bought A and B
+    SELECT customer_id
+    FROM orders
+    WHERE product_name in ('A', 'B')
+    GROUP BY customer_id
+    HAVING 
+        COUNT(DISTINCT product_name) = 2
+),
+c_customers AS (
+    -- customers who have bought C
+    SELECT DISTINCT(customer_id)
+    FROM orders
+    WHERE product_name = 'C'
+)
+SELECT c.customer_id, 
+    c.customer_name
+FROM customers c
+JOIN ab_customers ab
+    ON c.customer_id = ab.customer_id
+LEFT JOIN c_customers cc
+    ON ab.customer_id = cc.customer_id
+WHERE cc.customer_id IS NULL
+ORDER BY c.customer_name ASC
