848a0817ea0b4a7d9f6608159f7fa7557341a30b
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-04-04 10:16:57 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,31 @@
+class MinStack:
+    # initialize the stack object
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+    
+    # Push val onto the stack
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.min_stack.append(val)
+        
+        self.stack.append(val)
+        current_min = self.min_stack[-1]
+        self.min_stack.append(min(current_min, val))
+
+    # Remove the top element of the stack
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    # Get the top element of the stack
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    # Retrieve the minimum element in the stack
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+# Time Complexity: O(1)
+# Time Complexity: O(n)
