13d2115040329df0166b2aac9277595de676575c
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-04-08 20:53:27 -0400

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        '''
+        in:
+            - int[] temperatures
+            - temperatures[i] = daily temperatures on day i 
+        out:
+            - int[] result
+            - result[i] = num days until the temp is greater than on day i
+            - iff day i is the hottest, then result[i] = 0
+        constraints:
+            - 1 <= n <= 1000
+            - 1 <= temperatures[i] <= 100
+        '''
+        stack = []
+        result = [0] * len(temperatures)
+
+        for i, current in enumerate(temperatures):
+            while stack and stack[-1][0] < current:
+                value, index = stack.pop()
+                result[index] = i - index
+            
+            stack.append((current, i))
+        
+        return result
