ddc0b5c5cd0fdf7234f1896e9d39d6f18b4085b8
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-04-12 10:05:18 -0400

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        '''
+        in:
+            - int[] height
+        out:
+            - maximum area of water that can be trapped between the bars
+        constraints:
+            - 1 <= height.length <= 1000
+            - 0 <= height[i] <= 1000
+        edge cases:
+            - height.length <= 2 : return 0
+            - strictly increasing/decreasing : ret 0
+            - all equal : ret 0
+        '''
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+        total_water = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                left += 1
+                left_max = max(left_max, height[left])
+                total_water += left_max - height[left]
+            
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                total_water += right_max - height[right]
+        
+        return total_water
