2d17b565ad20aa142dc84e8b10479cc2b2860ed7
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-04-13 16:54:34 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        '''
+        in:
+            - int[] prices
+            - prices[i] = price of coin on day i
+        out:
+            - max profit you can achieve
+            - 0 allowed
+        constraints:
+            - can only buy or sell once on each day (not both)
+            - 1 <= prices.length <= 100
+            - 0 <= prices[i] <= 100
+        '''
+        buy, sell = 0, 1
+        max_profit = 0
+
+        while sell < len(prices):
+            if prices[buy] < prices[sell]:
+                curr_profit = prices[sell] - prices[buy]
+                max_profit = max(max_profit, curr_profit)
+            else:
+                buy = sell
+            
+            sell += 1
+
+        return max_profit
