c881083988f7bc281edd36a10742cf42caa061db
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-04-13 21:57:48 -0400

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.py
@@ -0,0 +1,31 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        '''
+        in:
+            - string s
+        out:
+            - string substring containing longest non repeating sequence of chars
+        constraints:
+            - 0 <= len(s) <= 1000
+            - s is ascii chars
+            - case sensitive?
+        edge cases:
+            - n = len(s)
+            - n == 0, 1
+        '''
+        left = 0
+        chars = set()
+        max_length = 0
+
+        for right in range(len(s)):
+            while s[right] in chars:
+                chars.remove(s[left])
+                left += 1
+            
+            chars.add(s[right])                
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
+
+# time complexity: O(n)
+# space complexity: O(m)
