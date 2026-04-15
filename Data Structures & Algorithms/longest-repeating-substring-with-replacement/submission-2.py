c572ec9a5516bbc3b5aab67cc0f71aae9c60bbc7
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-04-15 14:47:00 -0400

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        '''
+        in:
+            - string s
+            - int k
+        out:
+            - return len(longest substr) s.t.
+                - <= k replacements
+                - one distinct character
+        constraints:
+            - 1 <= len(s) <= 1000
+            - 0 <= k <= len(s)
+        '''
+        count = defaultdict(int)
+        max_length = 0
+
+        L = 0
+        for R in range(len(s)):
+            count[s[R]] += 1
+
+            while (R - L + 1) - max(count.values()) > k:
+                count[s[L]] -= 1
+                L += 1
+            
+            max_length = max(max_length, R - L + 1)
+        
+        return max_length
+    
+# time complexity: O(n)
+# space complexity: O(m), where m = 26, or the maximum num of unique chars
