5562a92f744ea29b225fec8ccf29c6178bb128cd
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-04-15 11:25:54 -0400

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        '''
+        in:
+            - int[] nums
+            - int k 
+        out:
+            - true iff 2 distinct indices i & j s.t.
+                - nums[i] == nums[j]
+                - abs(i - j) <= k
+            - false o/w
+        constraints:
+            - 1 <= nums.length <= 100,000
+            - -1,000,000,000 <= nums[i] <= 1,000,000,000
+            - 0 <= k <= 100,000
+        '''
+        window = set()
+
+        L = 0
+        for R in range(len(nums)):
+            if R - L > k:
+                window.remove(nums[L])
+                L += 1
+            
+            if nums[R] in window:
+                return True
+            else:
+                window.add(nums[R])
+                
+        return False
