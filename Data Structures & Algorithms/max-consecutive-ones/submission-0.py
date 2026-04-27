05ac76990f2066313e0dfda89a1487aef6506b73
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-04-27 16:31:03 -0400

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        '''
+        in:
+            - bin array nums
+        out:
+            - max num consecutive ones in the array
+        constraints:
+            - 1 <= n <= 100,000
+            - each val either 1 or 0
+        '''
+        res = 0
+        current = 0
+
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                current += 1
+            else:
+                res = max(res, current)
+                current = 0
+        
+        return max(current, res)
