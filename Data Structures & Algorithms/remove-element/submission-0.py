3df1da5533b63fa86d0dc2e226679828f6df62d3
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-04-27 16:48:34 -0400

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        '''
+        in:
+            - int[] nums
+            - int val
+        out:
+            - remove in place all occurences of val from nums
+            - return num of remaining elements k s.t.
+                - first k ele do not contain val
+        edge cases:
+            - 0 <= n <= 100
+            - 0 <= nums[i] <= 50
+            - 0 <= target <= 100
+        '''
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                del nums[i]
+                continue
+            
+            i += 1
+        
+        return len(nums)
