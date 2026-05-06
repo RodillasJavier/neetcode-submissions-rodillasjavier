1dcefe392cd0b25763987ef29ad23d5e115cd782
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-05-05 23:54:41 -0400

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        '''
+        in:
+            - int[] nums
+            - int k 
+        out:
+            - total num subarrays s.t. sum = k
+        constraints: 
+            - 1 <= n <= 20,000
+            - -1,000 <= nums[i] <= 1,000
+            - k can be positive or negative
+        '''
+        res = 0
+        current_sum = 0
+        prefix_sums = { 0 : 1 }
+
+        for num in nums:
+            current_sum += num
+            diff = current_sum - k
+
+            res += prefix_sums.get(diff, 0)
+            prefix_sums[current_sum] = prefix_sums.get(current_sum, 0) + 1
+        
+        return res
+
+
+'''
+nums = [2,-1,1,2], k = 2
+pref = [2,1,2,4]
+'''
