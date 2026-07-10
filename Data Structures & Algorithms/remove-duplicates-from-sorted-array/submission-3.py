bb9379f6627b6a747e114df85e58b9939c3da48b
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-07-10 13:32:51 -0700

    Add: remove-duplicates-from-sorted-array - submission-3

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        '''
+        in: 
+            - int array nums
+                - sorted non-decr.
+        out:
+            - remove duplicates from nums in-place
+                - s.t. each element appears only once
+            - return num of unique elements as k
+        '''
+        write = 1
+        for i in range(1, len(nums)):
+            if nums[i - 1] != nums[i]:
+                nums[write] = nums[i]
+                write += 1
+        
+        return write
+
+# time complexity O(n)
+# space complexity O(1)
