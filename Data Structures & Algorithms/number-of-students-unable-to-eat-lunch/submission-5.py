5aec4d0cbab05e757b6d5a3acbf4a1ee6ee8a8c8
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-07-28 18:40:03 -0700

    Add: number-of-students-unable-to-eat-lunch - submission-5

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-5.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        """
+        in: 
+            - array of students
+                - students[j] : preference of the j-th student in the initial queue
+            - stack of sandwiches
+                - sandwiches[i] : type of sandwich the ith one is
+        out:
+            - return number of students unable to eat
+        constraints:
+            - iff student @ front of q prefers sandwich:
+                - take it, leave q
+                - else, back of q
+            - until none of the students want the top sandwich
+        """
+        res = len(students)
+        count = Counter(students)
+
+        for sandwich in sandwiches:
+            if count[sandwich] > 0:
+                res -= 1
+                count[sandwich] -= 1
+            else:
+                return res
+
+        return res
+
+# time complexity: O(n)
+# space complexity: O(1)
