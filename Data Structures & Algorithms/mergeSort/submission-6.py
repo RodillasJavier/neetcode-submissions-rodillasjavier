d09865a0fc3740b4cf819105af2752d633841543
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-08-01 16:15:57 -0700

    Add: mergeSort - submission-6

---
+++ Data Structures & Algorithms/mergeSort/submission-6.py
@@ -0,0 +1,45 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def mergeSort(self, pairs: List[Pair]) -> List[Pair]:
+        """
+        in:
+            - list of (key, value) pairs
+        out:
+            - sorted list by key
+        constraints:
+            - use merge sort
+            - maintain relative order of two pairs w/the same key
+            - 0 <= pairs.length <= 100
+        """
+        # Base case: single element
+        if len(pairs) <= 1:
+            return pairs
+
+        mid = len(pairs) // 2
+        left = self.mergeSort(pairs[:mid])
+        right = self.mergeSort(pairs[mid:])
+        return self.merge(left, right)
+
+    def merge(self, left, right):
+        """
+        Merges two sorted arrays in non-desc order.
+        """
+        res = []
+        i, j = 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i].key <= right[j].key:
+                res.append(left[i])
+                i += 1
+            else:
+                res.append(right[j])
+                j += 1
+
+        res.extend(left[i:])
+        res.extend(right[j:])
+
+        return res
