0eca34e81e8a1b81b04d87ec18a847fefe2155b5
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-08-02 09:43:32 -0700

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.py
@@ -0,0 +1,34 @@
+from math import sqrt
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        """
+        in:
+            - 2d array points
+                - points[i] = [xi, yi] coordinates
+            - int k 
+        out:
+            - k closest points to the origin (0, 0)
+        constraints:
+            - return in any order
+            - answer guaranteed to be unique
+            - 1 <= k <= points.length <= 1000
+            - -100 <= points[i][0] <= points[i][i] <= 100
+        """
+        distances = []
+        for point in points:
+            entry = (self.distance(point), point)
+            distances.append(entry)
+        
+        distances.sort()
+
+        res = []
+        for i in range(k):
+            res.append(distances[i][1])
+        
+        return res
+
+    def distance(self, point):
+        x, y = point[0], point[1]
+
+        return sqrt((x) ** 2 + (y) ** 2)
