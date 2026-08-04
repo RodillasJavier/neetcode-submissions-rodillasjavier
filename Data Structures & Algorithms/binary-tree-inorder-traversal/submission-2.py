e000ae410e3e466cdeb9d59e07d483f88277f2fc
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-08-04 13:18:05 -0700

    Add: binary-tree-inorder-traversal - submission-2

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-2.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        """
+        in:
+            - root of a bin tree
+        out:
+            - return inorder traversal of it's nodes' values
+        constraints:
+            - 0 <= n <= 100
+            - node val can be pos or neg
+        """
+        res = []
+        stack = []
+
+        if not root:
+            return res
+        else:
+            stack.append((root, False))
+
+        while stack:
+            curr, visited = stack.pop()
+
+            if curr.right and not visited:
+                stack.append((curr.right, False))
+
+            if visited:
+                res.append(curr.val)
+            else:
+                stack.append((curr, True))
+
+            if curr.left and not visited:
+                stack.append((curr.left, False))
+        
+        return res
+
+# time complexity: O(n)
+# space complexity: O(n)
