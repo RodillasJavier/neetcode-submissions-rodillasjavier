19f85f500d271dca4f75075b33bc08a090030056
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-08-06 13:50:18 -0700

    Add: kth-largest-integer-in-a-stream - submission-7

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-7.py
@@ -0,0 +1,40 @@
+import heapq
+
+
+class KthLargest:
+    """
+    Class to find the kth largest integer in a stream of values, including
+    duplicates.
+
+    E.g. the 2nd largest from [1, 2, 3, 3] is 3.
+
+    constraints:
+        - 1 <= k <= 1000
+    """
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = []
+        self.k = k
+
+        for num in nums:
+            self.add(num)
+
+    def add(self, val: int) -> int:
+        """
+        Add the val to the stream and return the kth largest integer
+        """
+        # Case 1: The heap is full
+        if len(self.heap) >= self.k:
+            kth_largest = self.heap[0]
+
+            if kth_largest < val:
+                heapq.heapreplace(self.heap, val)
+        else:
+            # Case 2: The heap isn't full
+            heapq.heappush(self.heap, val)
+
+        return self.heap[0]
+
+
+# time complexity: O(n log k)
+# space complexity: O(k)
