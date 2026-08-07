bf2179cd7618b4087005cd2208969a77b58159ea
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-08-07 15:30:10 -0700

    Add: max-area-of-island - submission-4

---
+++ Data Structures & Algorithms/max-area-of-island/submission-4.py
@@ -0,0 +1,48 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        """
+        in:
+            - 2d matrix grid
+                - grid[i] = 0 => water
+                - grid[i] = 1 => land
+        out:
+            - return the maximum area of an island in grid
+            - if no island exists, return 0
+        """
+        ROWS, COLS = len(grid), len(grid[0])
+        DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        max_area = 0
+
+        to_visit = set()
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    to_visit.add((r, c))
+
+        def dfs(sr, sc):
+            if not 0 <= sr < ROWS or not 0 <= sc < COLS:
+                return 0
+            elif (sr, sc) not in to_visit:
+                return 0
+
+            to_visit.discard((sr, sc))
+
+            area = 1
+            for dr, dc in DIRECTIONS:
+                area += dfs(sr + dr, sc + dc)
+
+            return area
+
+        while to_visit:
+            sr, sc = next(iter(to_visit))
+
+            area = dfs(sr, sc)
+
+            max_area = max(area, max_area)
+
+        return max_area
+
+
+# time complexity: O(r * c)
+# space complexity: O(r * c)
