a83a973f8e090aa58128998a19072600cdfc0792
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-08-06 17:38:17 -0700

    Add: heap - submission-4

---
+++ Data Structures & Algorithms/heap/submission-4.py
@@ -0,0 +1,103 @@
+class MinHeap:
+    
+    def __init__(self):
+        self.heap = [None]
+
+    def push(self, val: int) -> None:
+        """
+        add val to the heap
+        """
+        self.heap.append(val)
+        i = len(self.heap) - 1
+
+        while i > 0:
+            parent = i // 2
+
+            if parent > 0 and self.heap[parent] > val:
+                self.heap[i], self.heap[parent] = self.heap[parent], self.heap[i]
+                i = parent
+            else:
+                break
+
+    def pop(self) -> int:
+        """
+        remove and return the smallest element in the heap. If the heap is 
+        empty, return -1
+        """
+        result = self.top()
+        if result == -1:
+            return result
+
+        i = 1
+        self.heap[1] = self.heap[-1]
+        self.heap.pop()
+        while i * 2 < len(self.heap):
+            if (
+                (i * 2) + 1 < len(self.heap) and 
+                self.heap[(i * 2) + 1] < self.heap[i * 2] and 
+                self.heap[(i * 2) + 1] < self.heap[i]
+            ):
+                # swapping with right child
+                child = (i * 2) + 1
+                self.heap[child], self.heap[i] = self.heap[i], self.heap[child]
+
+                i = child
+            elif (self.heap[i * 2] < self.heap[i]):
+                # swapping with left child
+                child = i * 2
+                self.heap[child], self.heap[i] = self.heap[i], self.heap[child]
+
+                i = child
+            else:
+                # Nodes are correctly ordered
+                break
+
+        return result
+
+    def top(self) -> int:
+        """
+        return the smallest element in the heap without removing it. If the 
+        heap is empty, return -1
+        """
+        if len(self.heap) <= 1:
+            return -1
+        else:
+            return self.heap[1]
+        
+
+    def heapify(self, nums: List[int]) -> None:
+        """
+        build a minimum heap from nums
+        """
+        if nums:
+            nums.append(nums[0])
+        self.heap = nums
+        curr = (len(self.heap) - 1) // 2
+
+        while curr > 0:
+            i = curr
+
+            while i * 2 < len(self.heap):
+                if (
+                    (i * 2) + 1 < len(self.heap) and 
+                    self.heap[(i * 2) + 1] < self.heap[i * 2] and 
+                    self.heap[(i * 2) + 1] < self.heap[i]
+                ):
+                    # swapping with right child
+                    child = (i * 2) + 1
+                    self.heap[child], self.heap[i] = self.heap[i], self.heap[child]
+
+                    i = child
+                elif (self.heap[i * 2] < self.heap[i]):
+                    # swapping with left child
+                    child = i * 2
+                    self.heap[child], self.heap[i] = self.heap[i], self.heap[child]
+
+                    i = child
+                else:
+                    # Nodes are correctly ordered
+                    break
+
+            curr -= 1
+
+        
