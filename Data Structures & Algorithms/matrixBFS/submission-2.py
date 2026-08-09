351da1d5794eff7972d68043a4660d848a477bbf
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-08-09 16:29:13 -0700

    Add: matrixBFS - submission-2

---
+++ Data Structures & Algorithms/matrixBFS/submission-2.py
@@ -0,0 +1,49 @@
+from collections import deque
+class Solution:
+    def shortestPath(self, grid: List[List[int]]) -> int:
+        """
+        in:
+            - binary tree matrix Grid
+                - 0 => land
+                - 1 => rocks (cannot be traversed)
+        out:
+            - return the length of the SP from origin to bottom right corner
+            - return -1 if there is not such path
+        constraints:
+            - all traversed cells are land cells (i.e. 0)
+            - only move vertically or horizontally
+            - length of SP is num of moves from starting cell to ending cell
+                - num cells - 1
+        """
+        ROWS, COLS = len(grid), len(grid[0])
+        DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        queue = deque()
+        visited = set()
+        length = 0
+
+        if grid[0][0] == 1 or grid[ROWS - 1][COLS - 1] == 1:
+            return -1
+        
+        queue.append((0, 0))
+
+        while queue:
+            for i in range(len(queue)):
+                r, c = queue.popleft()
+
+                if (r, c) == (ROWS - 1, COLS - 1):
+                    return length
+                
+                for dr, dc in DIRECTIONS:
+                    nr, nc = r + dr, c + dc
+                    if (
+                        0 <= nr < ROWS
+                        and 0 <= nc < COLS
+                        and grid[nr][nc] == 0
+                        and (nr, nc) not in visited
+                        ):
+                        queue.append((nr, nc))
+                        visited.add((nr, nc))
+            
+            length += 1
+
+        return -1
