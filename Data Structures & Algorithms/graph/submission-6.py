61a16831d82f77f94fdeaa5af2625e6720a2e025
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-08-10 09:12:19 -0700

    Add: graph - submission-6

---
+++ Data Structures & Algorithms/graph/submission-6.py
@@ -0,0 +1,60 @@
+from collections import deque
+
+
+class Graph:
+    def __init__(self):
+        """
+        init an empty directed graph
+        """
+        self.adj_list = {}
+
+    def addEdge(self, src: int, dst: int) -> None:
+        """
+        add an edge from src to dst if it DNE
+
+        if either src or dst exist, add them to the graph
+        """
+        if src not in self.adj_list:
+            self.adj_list[src] = set()
+        if dst not in self.adj_list:
+            self.adj_list[dst] = set()
+
+        self.adj_list[src].add(dst)
+
+    def removeEdge(self, src: int, dst: int) -> bool:
+        """
+        remove the edge from src to dst if it exists
+
+        return True iff the edge was removed else False
+        """
+        if src not in self.adj_list:
+            return False
+        if dst not in self.adj_list[src]:
+            return False
+
+        self.adj_list[src].discard(dst)
+        return True
+
+    def hasPath(self, src: int, dst: int) -> bool:
+        """
+        return whether there is a path from src to dst
+        """
+        queue = deque([src])
+        visited = set([src])
+
+        while queue:
+            current = queue.popleft()
+
+            if current == dst:
+                return True
+
+            for neighbor in self.adj_list[current]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+
+        return False
+
+
+# time complexity: O(V + E)
+# space complexity: O(V)
