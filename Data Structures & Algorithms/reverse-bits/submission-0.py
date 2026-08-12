9dde3d321d648d576406765590733ce19b451879
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-08-12 12:23:50 -0700

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        """
+        in:
+            - 32bit int
+        out:
+            - reverse the bits of the bin rep of n
+            - return the result as an integer
+        """
+        res = 0
+
+        for i in range(32):
+            bit = n % 2
+            n = n >> 1
+
+            
+            res += bit
+            
+            if i != 31:
+                res = res << 1
+
+        return res
+
+
+# time complexity: O(1)
+# space complexity: O(1)
