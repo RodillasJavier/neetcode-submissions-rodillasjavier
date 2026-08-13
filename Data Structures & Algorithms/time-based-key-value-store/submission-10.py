cb7d3598ac38e26cebe42138086fc4fddfa4e7bd
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-08-13 10:31:28 -0700

    Add: time-based-key-value-store - submission-10

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-10.py
@@ -0,0 +1,42 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        """
+        Store the key with value at the given timestamp
+        """
+        if key not in self.store:
+            self.store[key] = []
+        
+        self.store[key].append([timestamp, value])
+
+
+    def get(self, key: str, timestamp: int) -> str:
+        """
+        Return a value s.t.
+            - set was called previously w/timestamp_prev <= timestamp
+        
+        If there are multiple timestamps, return the most recent one
+        """
+        res = ""
+
+        if key not in self.store:
+            return res
+        
+        values = self.store[key]
+        
+        l, r = 0, len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+
+            if values[m][0] < timestamp:
+                res = values[m][1]
+                l = m + 1
+            elif values[m][0] == timestamp:
+                return values[m][1]
+            else:
+                r = m - 1
+        
+        return res
