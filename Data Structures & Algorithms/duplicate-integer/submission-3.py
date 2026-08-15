c7d1eb71be22050cac849ca49c12bc720aba8c9d
Javier Agustin Rodillas <103291127+RodillasJavier@users.noreply.github.com>
2026-08-15 06:26:42 -0700

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        """
+        in:
+            - int[] nums
+        out:
+            - true iff duplicates
+            - false o/w
+        """
+        seen = set()
+
+        for num in nums:
+            if num not in seen:
+                seen.add(num)
+            else:
+                return True
+
+        return False
+
+
+# time complexity: O(n)
+# space complexity: O(n)
